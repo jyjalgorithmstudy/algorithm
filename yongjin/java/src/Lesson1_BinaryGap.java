99b524b1c266932ed8d06bddccb4a3d7a299f04d
Ji Hwan Ma <jihwanma@Jiui-MacBookPro.local>
2019-02-14 00:17:49 +0900

    Lesson1_BinaryGap.java (using bitmask version)

--- yongjin/java/src/Lesson1_BinaryGap.java
+++ yongjin/java/src/Lesson1_BinaryGap.java
@@ -39,4 +39,35 @@ public class Lesson1_BinaryGap {
 
         return max;
     }
+    /*
+     bitmask version
+
+     public static int solution(int N) {
+        Boolean isStart = false;
+        int zeroCnt = 0;
+        int max = 0;
+
+        int bitmask = 1;
+
+        while (N > bitmask) {
+
+            if ((N & bitmask) == bitmask) {
+                isStart = true;
+
+                if (zeroCnt > max) {
+                    max = zeroCnt;
+                }
+
+                zeroCnt = 0;
+
+            } else if (isStart) {
+                zeroCnt++;
+            }
+
+            bitmask *= 2;
+        }
+
+        return max;
+    }
+    * */
 }
