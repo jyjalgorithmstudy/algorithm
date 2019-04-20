05d1cbd9cd8516665fe765a8523b1db629c5278f
Kimyongjin <yongjin5184@gmail.com>
2019-04-21 07:18:20 +0900

    Java - Codility Lesson 5. CountDiv

--- yongjin/java/src/Lesson5_CountDiv.java
+++ yongjin/java/src/Lesson5_CountDiv.java
@@ -4,7 +4,7 @@ public class Lesson5_CountDiv {
      *                두 정수 A, B 사이의 값들 중, K로 나누어 떨어지는 숫자의 갯수 구하는 문제
      * 초기
      * solution     : 1. for문을 사용 A부터 B까지 순회
-     *                 2. K로 나누어 떨어지는 경우에만 result++ 후 리턴
+     *                2. K로 나누어 떨어지는 경우에만 result++ 후 리턴
      *
      * complexity   : O(B-A) Performance tests 통과하지 못함. 50%
      */
@@ -13,8 +13,8 @@ public class Lesson5_CountDiv {
     /**
      * 개선
      * solution     : 1. 수학 공식을 이용
-     *                 2. A가 0인 경우, B / K + 1
-     *                 3. A가 0이 아닌 경우, B / K - (A - 1) / K
+     *                2. A가 0인 경우, B / K + 1
+     *                3. A가 0이 아닌 경우, B / K - (A - 1) / K
      *
      * complexity   : O(1) 100%
      */
